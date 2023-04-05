5851f689f27ae366a89aa51e2278beb631ba4126
hakunamatata <oullaiamine1@gmail.com>
2023-04-05 12:09:32 +0200

    deleted the Note Schema Example

--- src/index.js
+++ src/index.js
@@ -5,11 +5,5 @@ dotenv.config();
 const db = require("./db");
 async function main() {
   await db.connect();
-
-  const note = await db.NoteModel.create({
-    content: "This is my note's content",
-    title: "This is my title",
-  });
-  console.log(note);
 }
 main();
