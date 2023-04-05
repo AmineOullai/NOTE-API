3f1593fa3fad63581be99cd4df2e63d2fc185737
hakunamatata <oullaiamine1@gmail.com>
2023-04-05 14:39:25 +0200

    all of update delete create and find by id implement

--- src/routes/NoteRoutes.js
+++ src/routes/NoteRoutes.js
@@ -25,4 +25,73 @@ module.exports = (app) => {
       data: note,
     });
   });
+  app.get("/notes", async (req, res, next) => {
+    const notes = await NoteModel.find({});
+    return res.status(200).json({
+      message: "Notes retrieved",
+      data: notes,
+    });
+  });
+  app.get("/notes/:id", async (req, res, next) => {
+    const { id } = req.params;
+    const note = await NoteModel.findById(id);
+
+    if (!note)
+      return res.status(404).json({
+        message: "Note not found",
+      });
+    return res.status(200).json({
+      message: "Note retrieved",
+      data: note,
+    });
+  });
+  app.put("/notes/:id", async (req, res, next) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    if (!title && !content) {
+      return res.status(400).json({
+        message: "Title or Content are required",
+      });
+    }
+    if (title && title.trim().length > 50) {
+      return res.status(400).json({
+        message: "The title is too damn long",
+      });
+    }
+
+    const note = await NoteModel.findById(id);
+
+    if (!note)
+      return res.status(404).json({
+        message: "Note not found",
+      });
+
+    if (title) note.title = title.trim();
+
+    if (content) note.content = content.trim();
+
+    await note.save;
+
+    return res.status(200).json({
+      message: "Note updated",
+      data: note,
+    });
+  });
+  app.delete("/notes/:id", async (req, res, next) => {
+    const { id } = req.params;
+    const note = await NoteModel.findById(id);
+
+    if (!note)
+      return res.status(404).json({
+        message: "Note not found",
+      });
+
+    await NoteModel.deleteOne(note);
+
+    return res.status(200).json({
+      message: "Note deleted",
+      data: note,
+    });
+  });
 };
